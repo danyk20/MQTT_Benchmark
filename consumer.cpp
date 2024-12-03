6257594a49450669618e671e402f4b25f5cfe113
Daniel Košč <daniel.kosc@gmail.com>
2024-12-03 14:37:16 +0100

    Refactoring: Improve readability

--- consumer.cpp
+++ consumer.cpp
@@ -3,10 +3,16 @@
 #include <ostream>
 
 constexpr auto RESULTS_FILE = "consumer_results.txt";
-constexpr auto FILE_OPENNING = "[";
-constexpr auto FILE_CLOSSING = "]";
+constexpr int NUMBER_OF_MEASUREMENTS = 14; // number of different message payloads sizes (except separator)
+constexpr auto TOPIC = "test";
+constexpr auto CLIENT_ID = "";
 
 void store_string(const std::string &data) {
+    /**
+     * Save string into the file
+     *
+     * @data string to store
+     */
     std::ofstream outfile(RESULTS_FILE, std::ios_base::app);
 
     if (outfile.is_open()) {
@@ -18,7 +24,9 @@ void store_string(const std::string &data) {
 }
 
 std::string format_output(const std::vector<std::string> &strings) {
-    // [<measurement>,<measurement>,...,<measurement>]
+    /**
+     * Format elements in the vector into following string [<element>,<element>,...,<element>]
+     */
     std::string result = "[";
 
     for (size_t i = 0; i < strings.size(); ++i) {
@@ -32,51 +40,77 @@ std::string format_output(const std::vector<std::string> &strings) {
     return result;
 }
 
-[[noreturn]] int main() {
+void add_measurement(std::chrono::steady_clock::time_point starttime, long long received_messages, size_t current_size,
+                     std::vector<std::string> measurements) {
+    /*
+     * Add measurement as string into given vector of measurements. Format of single measurement is following :
+     * [number_of_messages,size_of_the_message,B/s,number_of_messages/s]
+     */
+    std::chrono::steady_clock::time_point endtime = std::chrono::steady_clock::now();
+    auto duration = endtime - starttime;
+    auto throughput = 1000000000 * received_messages * current_size / duration.count();
+    auto message_per_seconds = 1000000000 * received_messages / duration.count();
+    std::string measurement = "[" + std::to_string(received_messages) + "," + std::to_string(current_size) +
+                              "," + std::to_string(throughput) + "," + std::to_string(message_per_seconds)
+                              + "]";
+    measurements.push_back(measurement);
+}
+
+std::unique_ptr<mqtt::client> prepare_consumer() {
+    /**
+     * Create connection to the broker and subsribe to the topic based on global constants and enviromental variables
+     */
     const std::string brokerAddress = std::getenv("BROKER_IP");
     const int brokerPort = std::stoi(std::getenv("MQTT_PORT"));
     std::string broker = brokerAddress + ":" + std::to_string(brokerPort);
-    std::string id = "";
-    mqtt::client client(broker, id, mqtt::create_options(MQTTVERSION_5));
-    client.connect();
-    client.subscribe("test");
-    client.start_consuming();
-    auto starttime = std::chrono::steady_clock::now();
-    auto endtime = std::chrono::steady_clock::now();
+    auto client = std::make_unique<mqtt::client>(broker, CLIENT_ID, mqtt::create_options(MQTTVERSION_5));
+    client->connect();
+    client->subscribe(TOPIC);
+    client->start_consuming();
+    return client;
+}
+
+void process_payload(long long &received_messages, size_t &current_size, bool &separation,
+                     const mqtt::const_message_ptr &message_pointer) {
+    /**
+     * Read message size and update message counter. Empty message is considered as separator.
+     */
+    const std::string messageString = message_pointer->get_payload_str();
+    if (messageString.empty()) {
+        separation = true;
+    } else {
+        current_size = messageString.size();
+        separation = false;
+        received_messages++;
+    }
+}
+
+int main() {
+    const auto client = prepare_consumer().release();
+    auto start_time = std::chrono::steady_clock::now();
     long long received_messages = 0;
     size_t current_size = 0;
     std::vector<std::string> measurements;
     bool separation = false;
-    while (true) {
-        mqtt::const_message_ptr messagePointer;
+    mqtt::const_message_ptr messagePointer;
 
-        if (client.try_consume_message(&messagePointer)) {
-            std::string messageString = messagePointer->get_payload_str();
-            if (messageString.empty()) {
-                separation = true;
-            } else {
-                current_size = messageString.size();
-                separation = false;
-                received_messages++;
+    while (true) {
+        if (client->try_consume_message(&messagePointer)) {
+            // message arrived
+            process_payload(received_messages, current_size, separation, messagePointer);
+            if (received_messages > 0 && !separation) {
+                // start timer - first measured payload arrived
+                start_time = std::chrono::steady_clock::now();
+            } else if (separation && received_messages > 0) {
+                // stop timer - separator payload arrived
+                add_measurement(start_time, received_messages, current_size, measurements);
+                received_messages = 0; // reset message counter
             }
-            //std::cout << messageString.size() << std::endl;
-        }
-        if (received_messages == 1 && !separation) {
-            starttime = std::chrono::steady_clock::now();
-        } else if (separation && received_messages > 0) {
-            endtime = std::chrono::steady_clock::now();
-            auto duration = endtime - starttime;
-            auto throughput = 1000000000 * received_messages * current_size / duration.count();
-            auto message_per_seconds = 1000000000 * received_messages / duration.count();
-            std::string measurement = "[" + std::to_string(received_messages) + "," + std::to_string(current_size) +
-                                      "," + std::to_string(throughput) + "," + std::to_string(message_per_seconds)
-                                      + "]";
-            measurements.push_back(measurement);
-            received_messages = 0;
-        }
-        if (measurements.size() == 15) {
-            store_string(format_output(measurements));
-            measurements.clear();
+            if (measurements.size() == NUMBER_OF_MEASUREMENTS) {
+                store_string(format_output(measurements)); // save all measured data into file
+                measurements.clear(); // remove measured data for last batch (payloads with the same size)
+                return 0;
+            }
         }
     }
 }
