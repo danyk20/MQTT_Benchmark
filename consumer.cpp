ba21a48b8f5251e64365e98084f7d8e2a6e9fdee
Daniel Košč <daniel.kosc@gmail.com>
2024-12-09 14:39:46 +0100

    Fix: avoid access out of range

--- consumer.cpp
+++ consumer.cpp
@@ -30,10 +30,11 @@ std::string format_output(const std::vector<std::string> &strings) {
      */
     std::string result = "[";
 
-    for (size_t i = 0; i < strings.size(); ++i) {
-        result += strings[i];
-        if (i != strings.size() - 1) {
-            result += ",";
+    for (const auto& str : strings) {
+        if (&str != &strings.back()) {
+            result += str + ",";
+        } else {
+            result += str;
         }
     }
 
